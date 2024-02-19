04c1be2d15aa7f46534c8606d6921943c1391b0f
Wing <wing@busschots.ie>
2024-02-19 19:52:37 +0000

    Removed HTML files and replaced with JSON file

--- js/namereader.js
+++ js/namereader.js
@@ -1,84 +1,10 @@
 //IIFE
 (function() {
 
-    let names = {
-        feminine:[],
-        masculine:[]
-    };
 
     let $unsortedNames;
 
-    async function getFeminineNames() {
-        return fetch('/feminine.html')
-            .then(response => {
-                return response.text();
-            }).then(html => {
-                var parser = new DOMParser();
-                var doc = parser.parseFromString(html, "text/html");
-                var table = doc.querySelector('table');
-                let rows = table.getElementsByTagName('tr');
-                for (let row of rows)
-                {
-                    let link = row.querySelector('td > a');
-                    if (link)
-                    {
-                        addName('feminine', link.innerText);
-                    }
-                }
-                console.log("resolved fem");
-            });
-    }
-
-    function addName(list, name)
-    {
-        if(name.indexOf('/') >= 0)
-        {
-            let variants = name.split('/');
-            for(let variant of variants)
-            {
-                names[list].push(variant.trim());
-            }
-        }
-        else if(name.indexOf(',') >= 0)
-        {
-            let variants = name.split(',');
-            for(let variant of variants)
-            {
-                names[list].push(variant.trim());
-            }
-        }
-        else
-        {
-            names[list].push(name.trim());
-        }
-    }
-
-    async function getMasculineNames()
-    {
-        return fetch('/masculine.html')
-            .then(response=>{
-                return response.text();
-            })
-            .then(html=>{
-                var parser = new DOMParser();
-                var doc = parser.parseFromString(html, "text/html");
-                var list = doc.querySelector('ul');
-                var lis = list.getElementsByTagName('li');
-                for(let li of lis)
-                {
-                    let link = li.querySelector('a');
-                    if(link)
-                    {
-                        let name = link.innerText;
-                        addName("masculine", name);
-                    }
-                }
-                console.log("resolved masc");
-            });
-
-    }
-
-    function generateList()
+    function generateList(names)
     {
         let tenRandomMascNames = shuffle(names.masculine).slice(0, 10);
         let tenRandomFemNames = shuffle(names.feminine).slice(0,10);
@@ -111,12 +37,13 @@
 
     window.addEventListener('load', function () {
         $unsortedNames = document.getElementById('unsortedNames');
-        Promise.all([
-            getFeminineNames(),
-            getMasculineNames()
-        ]).then(()=>{
-            generateList()
-        });
+        fetch('./json/names.json')
+            .then((response)=>{
+                return response.json();
+            })
+            .then(namesJSON=>{
+                generateList(namesJSON);
+            });
 
     });
 })();
